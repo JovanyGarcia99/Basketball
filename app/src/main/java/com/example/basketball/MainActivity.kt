79ff371b731b18a5613825927328f5f4f75e498c
Jovany Garcia Alba <jogaal.2015@gmail.com>
2022-10-13 18:39:40 -0500

    git commit

---
+++ app/src/main/java/com/example/basketball/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.basketball
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+}
+
